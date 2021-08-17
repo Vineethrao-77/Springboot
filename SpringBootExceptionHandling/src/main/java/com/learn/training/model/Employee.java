de90c5d791a2fa451e8ff7f96cfc44ea92c131b3
Vineethrao-77 <88935971+Vineethrao-77@users.noreply.github.com>
2021-08-17 12:14:04 +0530

    Update Employee.java

--- SpringBootExceptionHandling/src/main/java/com/learn/training/model/Employee.java
+++ SpringBootExceptionHandling/src/main/java/com/learn/training/model/Employee.java
@@ -3,53 +3,74 @@ package com.learn.training.model;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.validation.constraints.NotBlank;
 
 @Entity
 @Table(name = "employees")
 public class Employee {
-@Id
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
 	private long id;
+	@NotBlank(message = "firstName is mandatory")
 	private String firstName;
+	@NotBlank(message = "lastName is mandatory")
 	private String lastName;
+	@NotBlank(message = "emailId is mandatory")
 	private String emailId;
 	private String designation;
+	@NotBlank(message = "age is mandatory")
 	private String age;
+
 	public long getId() {
 		return id;
 	}
+
 	public void setId(long id) {
 		this.id = id;
 	}
+
 	public String getFirstName() {
 		return firstName;
 	}
+
 	public void setFirstName(String firstName) {
 		this.firstName = firstName;
 	}
+
 	public String getLastName() {
 		return lastName;
 	}
+
 	public void setLastName(String lastName) {
 		this.lastName = lastName;
 	}
+
 	public String getEmailId() {
 		return emailId;
 	}
+
 	public void setEmailId(String emailId) {
 		this.emailId = emailId;
 	}
+
 	public String getDesignation() {
 		return designation;
 	}
+
 	public void setDesignation(String designation) {
 		this.designation = designation;
 	}
+
 	public String getAge() {
 		return age;
 	}
+
 	public void setAge(String age) {
 		this.age = age;
 	}
+
 	public Employee(long id, String firstName, String lastName, String emailId, String designation, String age) {
 		super();
 		this.id = id;
@@ -59,8 +80,9 @@ public class Employee {
 		this.designation = designation;
 		this.age = age;
 	}
+
 	public Employee() {
 		super();
 	}
-	
+
 }
