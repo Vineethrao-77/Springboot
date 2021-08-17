97098f90dd97c386b64c5cc84e9fa19bdd484de2
Vineethrao-77 <88935971+Vineethrao-77@users.noreply.github.com>
2021-08-17 12:10:53 +0530

    Update GlobalExceptionHandler.java

--- SpringBootExceptionHandling/src/main/java/com/learn/training/Exceptions/GlobalExceptionHandler.java
+++ SpringBootExceptionHandling/src/main/java/com/learn/training/Exceptions/GlobalExceptionHandler.java
@@ -21,6 +21,13 @@ public class GlobalExceptionHandler {
 		ErrorDetails errorDetails = new ErrorDetails(new Date(), ex.getMessage(), request.getDescription(false));
 		return new ResponseEntity<>(errorDetails, HttpStatus.NOT_FOUND);
 	}
+	@ExceptionHandler(EmployeMailException.class)
+	
+	public ResponseEntity<?> EmployeMailException (EmployeMailException ex, WebRequest request){
+		ErrorDetails errorDetails= new ErrorDetails(new Date(), ex.getLocalizedMessage(),request.getDescription(false));
+		return new ResponseEntity<>(errorDetails, HttpStatus.NOT_FOUND);
+}
+	
 	
 	@ExceptionHandler(Exception.class)
 	public ResponseEntity<?> globleExcpetionHandler(Exception ex, WebRequest request) {
