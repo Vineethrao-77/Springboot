354aafe0dc3d6498efabb8dc4046ac6496cf8f7e
Vineethrao-77 <88935971+Vineethrao-77@users.noreply.github.com>
2021-08-17 12:09:52 +0530

    added employeeMailException

---
+++ SpringBootExceptionHandling/src/main/java/com/learn/training/Exceptions/EmployeMailException.java
@@ -0,0 +1,16 @@
+package com.learn.training.Exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+@ResponseStatus(value = HttpStatus.NOT_FOUND)
+public class EmployeMailException extends RuntimeException {
+	
+		
+			private static final long serialVersionUID = 1L;
+
+			public EmployeMailException(String message){
+				super(message);
+		    	
+		    }
+		
+}
